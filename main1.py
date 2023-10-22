8837ffa36733ddb2734dc72f428d4d938261c9e7
Tom Black <tom.black06007@gmail.com>
2023-10-21 23:02:11 -0600

    worked on more rust today.

--- main1.py
+++ main1.py
@@ -80,6 +80,7 @@ def getTodo(todo_id: int):
 # End point to handle the deletion of a record.
 # End point to handle the deletion of a record.
 # worked on rust today.
+# worked on more rust today.
 @app.delete("/todos/{todo_id}/")
 def delete_todo(todo_id: int):
     try:
